a0399209e4df1232aa8fc6be379af67213962455
raulsposito <raulsposito@gmail.com>
2021-04-22 16:35:07 -0300

    refactor card controller

--- app/controllers/api/v1/card_controller.rb
+++ app/controllers/api/v1/card_controller.rb
@@ -1,12 +1,25 @@
 module Api
   module V1 
     class CardController < ApplicationController
+      before_action :set_card, only: [:show]
+
       def index
         @cards = Card.all
+        render json: @cards
       end
     
       def show
-        @card = Card.find_by_id(params[:id])
+        render json: @card 
+      end
+
+      private 
+
+      def set_card
+        @card = Card.find(params[:index])
+      end
+
+      def card_params
+        params.require(:card).permit(:index, :title)
       end
     end
   end
