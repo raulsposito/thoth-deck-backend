cd2beb4d06b94fda4d229f0881101c6f50c53110
raulsposito <raulsposito@gmail.com>
2021-04-29 16:25:14 -0300

    rm commented code

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,14 +1,3 @@
 class Card < ApplicationRecord
-
-    
-#   scope :for_dates,                 -> (start_date, end_date) { where("account_reports.created_at BETWEEN ? AND ?", start_date, end_date + 1.days) }
-#   scope :custom,                    -> { where(custom: true) }
-#   scope :draft,                     -> { where(report_status: 'Draft') }
-#   scope :published,                 -> { where(report_status: 'Published!') }
-#   scope :by_subscription_status,    -> (status) { joins(:account).where('accounts.subscription_status': status) }
-#   scope :by_account_id,             -> (account_id) { where(account_id: account_id )}
-
-    # scope :search_input, -> (query) { where("title LIKE :query", query: "%#{query.strip.downcase}%")}
-
   scope :search_input, -> (query) { where('(LOWER(cards.title) LIKE :query)', query: "%#{query.strip.downcase}%")}
 end
